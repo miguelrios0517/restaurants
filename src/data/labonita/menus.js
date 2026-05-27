442d183ccd9333d0104b4b81296de4b0a94e08b2
Miguel Rios <miguel.rios1997@gmail.com>
2026-05-27 11:48:19 -0700

    created la bonita grocery

---
+++ src/data/labonita/menus.js
@@ -0,0 +1,56 @@
+export const menuMain = [
+  {
+    name: "Home",
+    url: "/labonita/",
+    highlighted: false,
+  },
+  {
+    name: "Butcher",
+    url: "/labonita/butcher",
+    highlighted: false,
+  },
+  {
+    name: "Restaurant",
+    url: "/labonita/restaurant",
+    highlighted: false,
+  },
+  {
+    name: "Gallery",
+    url: "/labonita/gallery",
+    highlighted: false,
+  },
+  {
+    name: "Hours & Location",
+    url: "/labonita/contact",
+    highlighted: false,
+  },
+  {
+    name: "Call Us",
+    url: "/labonita/book",
+    highlighted: true,
+  },
+];
+
+export const footerPrimary = [
+  {
+    title: "Navigatie",
+    items: [{ name: "home", url: "/" }],
+  },
+];
+
+export const footerSecondary = [
+  {
+    title: "support",
+    items: [{ name: "pricing", url: "/pricing" }],
+  },
+];
+
+export const footerTertiary = [
+  {
+    title: "Links",
+    items: [
+      { name: "privacybeleid", url: "/privacy-policy" },
+      { name: "api status", url: "/api-status" },
+    ],
+  },
+];
