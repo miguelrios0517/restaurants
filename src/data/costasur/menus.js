1b6a3ba736675c22e904d1ea19baea4d7a733d1b
Miguel Rios <miguel.rios1997@gmail.com>
2026-05-28 00:50:01 -0700

    fixed fonts in style

---
+++ src/data/costasur/menus.js
@@ -0,0 +1,56 @@
+export const menuMain = [
+  {
+    name: "Home",
+    url: "/costasur/",
+    highlighted: false,
+  },
+  {
+    name: "Menu",
+    url: "/costasur/menu",
+    highlighted: false,
+  },
+  {
+    name: "About Us",
+    url: "/costasur/about-us",
+    highlighted: false,
+  },
+  {
+    name: "Gallery",
+    url: "/costasur/gallery",
+    highlighted: false,
+  },
+  {
+    name: "Hours & Location",
+    url: "/costasur/contact",
+    highlighted: false,
+  },
+  {
+    name: "(123) 456-789",
+    url: "/costasur/book",
+    highlighted: true,
+  },
+];
+
+export const footerPrimary = [
+  {
+    title: "Navigatie",
+    items: [{ name: "home", url: "/" }],
+  },
+];
+
+export const footerSecondary = [
+  {
+    title: "support",
+    items: [{ name: "pricing", url: "/pricing" }],
+  },
+];
+
+export const footerTertiary = [
+  {
+    title: "Links",
+    items: [
+      { name: "privacybeleid", url: "/privacy-policy" },
+      { name: "api status", url: "/api-status" },
+    ],
+  },
+];
