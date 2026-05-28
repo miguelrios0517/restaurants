1e5e49c2b29a5abb33bf6b15d1dad18a011cd1f3
Miguel Rios <miguel.rios1997@gmail.com>
2026-05-27 22:49:20 -0700

    fixed theme colors in config

--- src/data/labonita/config.ts
+++ src/data/labonita/config.ts
@@ -4,7 +4,7 @@ export const siteCurrency = "USD";
 export const siteDomain = "astropie.com";
 export const siteName = "AstroPie";
 export const siteSlogan = "The best pies in the galaxy!";
-export const themeColor = "#FF0000";
+export const themeColor = "#FF007B";
 
 export const phone = {
   href: "tel:+123456789",
