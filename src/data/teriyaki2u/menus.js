0379a06bdcbb8a0cbeeacafc3d162d967bd7369f
Miguel Rios <miguel.rios1997@gmail.com>
2026-05-30 14:01:00 -0700

    added teriyaki 2u

---
+++ src/data/teriyaki2u/menus.js
@@ -0,0 +1,56 @@
+export const menuMain = [
+  {
+    name: "Home",
+    url: "/teriyaki2u/",
+    highlighted: false,
+  },
+  {
+    name: "Menu",
+    url: "/teriyaki2u/menu",
+    highlighted: false,
+  },
+  {
+    name: "About Us",
+    url: "/teriyaki2u/about-us",
+    highlighted: false,
+  },
+  {
+    name: "Gallery",
+    url: "/teriyaki2u/gallery",
+    highlighted: false,
+  },
+  {
+    name: "Hours & Location",
+    url: "/teriyaki2u/contact",
+    highlighted: false,
+  },
+  {
+    name: "(123) 456-789",
+    url: "/teriyaki2u/book",
+    highlighted: true,
+  },
+];
+
+export const footerPrimary = [
+  {
+    title: "Navigatie",
+    items: [{ name: "home", url: "/" }],
+  },
+];
+
+export const footerSecondary = [
+  {
+    title: "support",
+    items: [{ name: "pricing", url: "/pricing" }],
+  },
+];
+
+export const footerTertiary = [
+  {
+    title: "Links",
+    items: [
+      { name: "privacybeleid", url: "/privacy-policy" },
+      { name: "api status", url: "/api-status" },
+    ],
+  },
+];
